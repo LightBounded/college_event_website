17bbf28804d489123216e2a4769b95b4256a338a
Caleb Rivera <calebrossr@gmail.com>
2024-03-23 07:02:01 -0400

    Add supported school domains consts

---
+++ src/consts/index.ts
@@ -0,0 +1,12 @@
+export const SUPPORTED_SCHOOL_DOMAINS = [
+  "ucf.edu",
+  "uf.edu",
+  "fsu.edu",
+  "usf.edu",
+  "fiu.edu",
+  "famu.edu",
+  "unf.edu",
+  "uwf.edu",
+  "fgcu.edu",
+  "fau.edu",
+];
