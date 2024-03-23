bb70e6b07e0d2b18fc02b5fae65f6f7f33efed42
Caleb Rivera <calebrossr@gmail.com>
2024-03-23 07:01:40 -0400

    Move home page into main route group

---
+++ src/app/(main)/page.tsx
@@ -0,0 +1,3 @@
+export default async function Home() {
+  return <main className="flex min-h-screen flex-col p-4">Hello, World!</main>;
+}
--- src/app/page.tsx
+++
@@ -1,3 +0,0 @@
-export default async function Home() {
-  return <main className="flex min-h-screen flex-col">Hello, World!</main>;
-}
