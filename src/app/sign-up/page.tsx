8968f2b7cc5a2bdf6295648815574ac71d85f2da
Caleb Rivera <calebrossr@gmail.com>
2024-03-23 06:35:26 -0400

    Create sign up form

---
+++ src/app/sign-up/page.tsx
@@ -0,0 +1,9 @@
+import { SignUpForm } from "./sign-up-form";
+
+export default function SignUp() {
+  return (
+    <div className="mx-auto flex min-h-screen max-w-80 flex-col items-center justify-center">
+      <SignUpForm />
+    </div>
+  );
+}
---
+++ src/app/sign-up/sign-up-form.tsx
@@ -0,0 +1,74 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+import { Button } from "~/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+  useForm,
+} from "~/components/ui/form";
+import { Input } from "~/components/ui/input";
+import { api } from "~/trpc/react";
+import { SignUpSchema } from "~/validators/auth";
+
+export function SignUpForm() {
+  const router = useRouter();
+  const form = useForm({
+    schema: SignUpSchema,
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const signUp = api.auth.signUp.useMutation();
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(async (values) => {
+          signUp.mutate(values);
+          router.push("/");
+        })}
+        className="flex w-full flex-col gap-4"
+      >
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>School Email</FormLabel>
+              <FormControl>
+                <Input
+                  placeholder="School Email"
+                  autoComplete="email"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input placeholder="Password" type="password" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button>Sign Up</Button>
+      </form>
+    </Form>
+  );
+}
