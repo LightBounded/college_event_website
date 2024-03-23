86ca1207108befe2cc29c9ec6451097ef59122d3
Caleb Rivera <calebrossr@gmail.com>
2024-03-23 06:52:51 -0400

    Redirect user to home from sign up if they're signed in

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -1,6 +1,13 @@
+import { redirect } from "next/navigation";
+
+import { validateRequest } from "~/server/auth/validate-request";
 import { SignUpForm } from "./sign-up-form";
 
-export default function SignUp() {
+export default async function SignUp() {
+  const { user } = await validateRequest();
+
+  if (user) redirect("/");
+
   return (
     <div className="mx-auto flex min-h-screen max-w-80 flex-col items-center justify-center">
       <SignUpForm />
