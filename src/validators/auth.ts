2992a2ac87ddfdfd186e9f5fbdd7c67cc0179cfd
Caleb Rivera <calebrossr@gmail.com>
2024-03-23 07:02:11 -0400

    Add domain validation to email field in SignUpSchema

--- src/validators/auth.ts
+++ src/validators/auth.ts
@@ -1,7 +1,15 @@
 import { z } from "zod";
 
+import { SUPPORTED_SCHOOL_DOMAINS } from "~/consts";
+
 export const SignUpSchema = z.object({
-  email: z.string().email(),
+  email: z
+    .string()
+    .email()
+    .refine((email) => {
+      const domain = email.split("@")[1]!; // Email must have a domain
+      return SUPPORTED_SCHOOL_DOMAINS.includes(domain);
+    }),
   password: z.string().min(8, "Password must be at least 8 characters"),
 });
 
