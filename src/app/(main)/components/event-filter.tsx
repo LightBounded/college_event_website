91e0d01844d84d653f57f659bc2ce969efb3d56b
Pharit Smitasin <pharitsmitasin@gmail.com>
2024-04-03 19:45:50 -0400

    delete filter by category for events/orgs

--- src/app/(main)/components/event-filter.tsx
+++ src/app/(main)/components/event-filter.tsx
@@ -1,36 +1,11 @@
-import { Label } from "~/components/ui/label";
-import { RadioGroup, RadioGroupItem } from "~/components/ui/radio-group";
+import { RadioGroup } from "~/components/ui/radio-group";
 import Eventsearch from "./event-search";
 
 export default async function Eventfilter() {
   return (
-    <RadioGroup className="flex flex-col">
-      <div className="mb-4 flex items-center space-x-2">
+    <RadioGroup defaultValue="option-one" className="flex flex-col">
+      <div className="mb-4 flex flex-col items-center space-x-2">
         <Eventsearch />
-      </div>
-      <div className="flex items-center space-x-2">
-        <RadioGroupItem value="option-one" id="option-one" />
-        <Label htmlFor="option-one">Social</Label>
-      </div>
-      <div className="flex items-center space-x-2">
-        <RadioGroupItem value="option-two" id="option-two" />
-        <Label htmlFor="option-two">Athletic</Label>
-      </div>
-      <div className="flex items-center space-x-2">
-        <RadioGroupItem value="option-three" id="option-three" />
-        <Label htmlFor="option-three">Entertainment</Label>
-      </div>
-      <div className="flex items-center space-x-2">
-        <RadioGroupItem value="option-four" id="option-four" />
-        <Label htmlFor="option-four">Educational</Label>
-      </div>
-      <div className="flex items-center space-x-2">
-        <RadioGroupItem value="option-five" id="option-five" />
-        <Label htmlFor="option-five">Spiritual/Religious</Label>
-      </div>
-      <div className="flex items-center space-x-2">
-        <RadioGroupItem value="option-six" id="option-six" />
-        <Label htmlFor="option-six">Fundraising</Label>
       </div>
     </RadioGroup>
   );
