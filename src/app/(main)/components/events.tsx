ca1c3db2d56c91e9e198eaf0c5af150ccfa88528
Pharit Smitasin <pharitsmitasin@gmail.com>
2024-04-03 19:31:16 -0400

    update card width for smaller viewports

---
+++ src/app/(main)/components/events.tsx
@@ -0,0 +1,46 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
+
+export default async function Events() {
+  return (
+    <>
+      {events.map((organization) => (
+        <Card key={organization.name} className="w-[300px] lg:w-[600px]">
+          <CardHeader>
+            <CardTitle>{organization.name}</CardTitle>
+            <CardDescription>{organization.description}</CardDescription>
+          </CardHeader>
+        </Card>
+      ))}
+    </>
+  );
+}
+
+const events = [
+  {
+    name: "Event Name 1",
+    description: "Event Description 1",
+  },
+  {
+    name: "Event Name 2",
+    description: "Event Description 2",
+  },
+  {
+    name: "Event Name 3",
+    description: "Event Description 3",
+  },
+  {
+    name: "Event Name 4",
+    description: "Event Description 4",
+  },
+  {
+    name: "Event Name 5",
+    description: "Event Description 5",
+  },
+];
---
+++ src/app/(main)/components/organizations.tsx
@@ -0,0 +1,51 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
+
+export default async function Organizations() {
+  return (
+    <>
+      {organizations.map((organization) => (
+        <Card key={organization.name} className="w-[300px] lg:w-[600px]">
+          <CardHeader>
+            <CardTitle>{organization.name}</CardTitle>
+            <CardDescription>{organization.description}</CardDescription>
+          </CardHeader>
+        </Card>
+      ))}
+    </>
+  );
+}
+
+const organizations = [
+  {
+    name: "Club Name 1",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    name: "Club Name 2",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    name: "Club Name 3",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    name: "Club Name 4",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    name: "Club Name 5",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+];
