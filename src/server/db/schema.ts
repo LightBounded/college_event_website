dbcaab60ea21ef6b176f076e82affd71908ff7b9
Caleb Rivera <calebrossr@gmail.com>
2024-04-03 18:14:14 -0400

    Create database schema

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,6 +1,7 @@
 // Example model schema from the Drizzle docs
 // https://orm.drizzle.team/docs/sql-schema-declaration
 
+import { sql } from "drizzle-orm";
 import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 // export const posts = sqliteTable(
@@ -33,4 +34,84 @@ export const sessions = sqliteTable("session", {
     .notNull()
     .references(() => users.id),
   expiresAt: int("expires_at").notNull(),
+});
+
+export const universities = sqliteTable("university", {
+  id: text("id").notNull().primaryKey(),
+  adminId: text("admin_id")
+    .notNull()
+    .references(() => users.id),
+  name: text("name").notNull(),
+  description: text("description"),
+  studentsCount: int("students_count").notNull().default(0),
+});
+
+export const location = sqliteTable("location", {
+  id: text("id").notNull().primaryKey(),
+  name: text("name").notNull(),
+  longtitude: int("longtitude").notNull(),
+  latitude: int("latitude").notNull(),
+});
+
+export const organizations = sqliteTable("organization", {
+  id: text("id").notNull().primaryKey(),
+  name: text("name").notNull(),
+  ownerId: text("owner_id")
+    .notNull()
+    .references(() => users.id),
+  description: text("description"),
+  membersCount: int("members_count").notNull().default(0),
+});
+
+export const members = sqliteTable("member", {
+  id: text("id").notNull().primaryKey(),
+  userId: text("user_id")
+    .notNull()
+    .references(() => users.id),
+  organizationId: text("organization_id")
+    .notNull()
+    .references(() => organizations.id),
+});
+
+export const events = sqliteTable("event", {
+  id: text("id").notNull().primaryKey(),
+  organizationId: text("organization_id")
+    .notNull()
+    .references(() => organizations.id),
+  name: text("name").notNull(),
+  description: text("description"),
+  locationId: text("location_id")
+    .notNull()
+    .references(() => location.id),
+  time: text("time").notNull(),
+  date: text("date").notNull(),
+  membersCount: int("members_count").notNull().default(0),
+});
+
+export const comments = sqliteTable("comment", {
+  id: text("id").notNull().primaryKey(),
+  eventId: text("event_id")
+    .notNull()
+    .references(() => events.id),
+  userId: text("user_id")
+    .notNull()
+    .references(() => users.id),
+  text: text("text").notNull(),
+  time: text("time")
+    .notNull()
+    .default(sql`CURRENT_TIMESTAMP`),
+});
+
+export const ratings = sqliteTable("rating", {
+  id: text("id").notNull().primaryKey(),
+  eventId: text("event_id")
+    .notNull()
+    .references(() => events.id),
+  userId: text("user_id")
+    .notNull()
+    .references(() => users.id),
+  value: int("value").notNull(),
+  time: text("time")
+    .notNull()
+    .default(sql`CURRENT_TIMESTAMP`),
 });
