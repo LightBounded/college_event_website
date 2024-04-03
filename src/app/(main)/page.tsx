7eb5c02cdd210f433364d1e701832c9056a43b1c
Pharit Smitasin <pharitsmitasin@gmail.com>
2024-04-02 23:17:54 -0400

    add tabs for organization and event view

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,3 +1,22 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "~/components/ui/tabs";
+import Events from "./events";
+import Organizations from "./organizations";
+
 export default async function Home() {
-  return <main className="flex min-h-screen flex-col p-4">Hello, World!</main>;
+  return (
+    <main className="flex min-h-screen flex-col p-4">
+      <Tabs defaultValue="account" className="w-[400px]">
+        <TabsList>
+          <TabsTrigger value="account">Organizations</TabsTrigger>
+          <TabsTrigger value="password">Events</TabsTrigger>
+        </TabsList>
+        <TabsContent value="account">
+          <Organizations />
+        </TabsContent>
+        <TabsContent value="password">
+          <Events />
+        </TabsContent>
+      </Tabs>
+    </main>
+  );
 }
