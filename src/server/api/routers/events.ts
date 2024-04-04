eff2f5dea20573dc6f0908c6027e20f120504c9b
farbubby <farhan.mahbub@ucf.edu>
2024-04-03 22:47:33 -0400

    Added procedures that create, update, and delete events

--- src/server/api/routers/events.ts
+++ src/server/api/routers/events.ts
@@ -1,7 +1,34 @@
-import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { eq } from "drizzle-orm";
+import { z } from "zod";
+
+import {
+  createTRPCRouter,
+  organizationAdminProcedure,
+  publicProcedure,
+} from "~/server/api/trpc";
+import { events } from "~/server/db/schema";
+import { CreateEventSchema, UpdateEventSchema } from "~/validators/events";
 
 export const eventsRouter = createTRPCRouter({
   all: publicProcedure.query(async ({ ctx }) => {
     return await ctx.db.query.events.findMany();
   }),
+  createEvent: organizationAdminProcedure
+    .input(CreateEventSchema)
+    .mutation(async ({ input, ctx }) => {
+      await ctx.db.insert(events).values(input);
+    }),
+  updateEvent: organizationAdminProcedure
+    .input(UpdateEventSchema)
+    .mutation(async ({ input, ctx }) => {
+      await ctx.db
+        .update(events)
+        .set(input)
+        .where(eq(events.id, input.eventId));
+    }),
+  deleteEvent: organizationAdminProcedure
+    .input(z.object({ eventId: z.number() }))
+    .mutation(async ({ input, ctx }) => {
+      await ctx.db.delete(events).where(eq(events.id, input.eventId));
+    }),
 });
