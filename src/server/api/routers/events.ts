d204c63b50eb6536353b5f340f7317ecd72a7e6a
farbubby <farhan.mahbub@ucf.edu>
2024-04-03 21:08:56 -0400

    Created procedure that creates events

--- src/server/api/routers/events.ts
+++ src/server/api/routers/events.ts
@@ -1,6 +1,11 @@
 import { TRPCError } from "@trpc/server";
+import { z } from "zod";
 
-import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import {
+  createTRPCRouter,
+  organizationAdminProcedure,
+  publicProcedure,
+} from "~/server/api/trpc";
 import { events } from "~/server/db/schema";
 
 export const eventsRouter = createTRPCRouter({
@@ -16,4 +21,20 @@ export const eventsRouter = createTRPCRouter({
 
     return eventsList;
   }),
+  createEvent: organizationAdminProcedure
+    .input(
+      // Abstract the input schema into a zod schema in the validators folder
+      z.object({
+        name: z.string(),
+        description: z.string(),
+        locationId: z.string(),
+        time: z.string(),
+        date: z.string(),
+        contactEmail: z.string(),
+        contactPhone: z.string(),
+      }),
+    )
+    .mutation(async ({ input, ctx }) => {
+      await ctx.db.insert(events).values(input);
+    }),
 });
