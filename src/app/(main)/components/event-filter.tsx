eab0543769c2c2c80d4200e15eccbbaae49f7450
Pharit Smitasin <pharitsmitasin@gmail.com>
2024-04-03 20:00:35 -0400

    adjust spacing below filter components

--- src/app/(main)/components/event-filter.tsx
+++ src/app/(main)/components/event-filter.tsx
@@ -4,7 +4,7 @@ import Eventsearch from "./event-search";
 export default async function Eventfilter() {
   return (
     <RadioGroup defaultValue="option-one" className="flex flex-col">
-      <div className="mb-4 flex flex-col items-center space-x-2">
+      <div className="flex items-center space-x-2">
         <Eventsearch />
       </div>
     </RadioGroup>
--- src/app/(main)/components/org-filter.tsx
+++ src/app/(main)/components/org-filter.tsx
@@ -3,8 +3,8 @@ import Orgsearch from "./org-search";
 
 export default async function Orgfilter() {
   return (
-    <RadioGroup className="flex flex-col">
-      <div className="mb-4 flex items-center space-x-2">
+    <RadioGroup defaultValue="option-one" className="flex flex-col">
+      <div className="flex items-center space-x-2">
         <Orgsearch />
       </div>
     </RadioGroup>
