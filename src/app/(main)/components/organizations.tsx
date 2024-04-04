bfdea026a1a404aff4f2f23890c0f3b3a19c5319
Pharit Smitasin <pharitsmitasin@gmail.com>
2024-04-03 22:27:31 -0400

    add org IDs to placeholder orgs

--- src/app/(main)/components/organizations.tsx
+++ src/app/(main)/components/organizations.tsx
@@ -22,28 +22,33 @@ export default async function Organizations() {
 
 const organizations = [
   {
-    name: "Club Name 1",
+    name: "Meow Club",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    id: 1,
   },
   {
     name: "Club Name 2",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    id: 2,
   },
   {
     name: "Club Name 3",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    id: 3,
   },
   {
     name: "Club Name 4",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    id: 4,
   },
   {
     name: "Club Name 5",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    id: 5,
   },
 ];
