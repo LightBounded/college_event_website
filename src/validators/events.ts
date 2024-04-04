112e650f519c165ad5cde2319b5c964cfe49b09f
farbubby <farhan.mahbub@ucf.edu>
2024-04-03 22:31:11 -0400

    Abstracted input schemas into zod schemas

---
+++ src/validators/events.ts
@@ -0,0 +1,26 @@
+import { z } from "zod";
+
+export const CreateEventSchema = z.object({
+  name: z.string(),
+  description: z.string(),
+  locationId: z.number(),
+  time: z.string(),
+  date: z.string(),
+  contactEmail: z.string(),
+  contactPhone: z.string(),
+});
+
+export const UpdateEventSchema = z.object({
+  eventId: z.number(),
+  name: z.string(),
+  description: z.string(),
+  locationId: z.number(),
+  time: z.string(),
+  date: z.string(),
+  contactEmail: z.string(),
+  contactPhone: z.string(),
+});
+
+export const DeleteEventSchema = z.object({
+  eventId: z.number(),
+});
