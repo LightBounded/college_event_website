390b4c87bdd819463ba6d786d9cb06f1a51b76fc
Pharit Smitasin <pharitsmitasin@gmail.com>
2024-04-04 18:21:57 -0400

    update eventsearch to camelcase

--- src/app/(main)/components/event-filter.tsx
+++ src/app/(main)/components/event-filter.tsx
@@ -1,11 +1,11 @@
 import { RadioGroup } from "~/components/ui/radio-group";
-import Eventsearch from "./event-search";
+import EventSearch from "./event-search";
 
 export default async function Eventfilter() {
   return (
     <RadioGroup defaultValue="option-one" className="flex flex-col">
       <div className="flex items-center space-x-2">
-        <Eventsearch />
+        <EventSearch />
       </div>
     </RadioGroup>
   );
