62901d0b499848b253614abef4d38692dd3e1063
Pharit Smitasin <pharitsmitasin@gmail.com>
2024-04-06 01:58:56 -0400

    add orgs from db

---
+++ src/app/(main)/components/organizations-list.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import { use, useState } from "react";
+import Link from "next/link";
+
+import type { SCHOOLS } from "~/consts";
+import type { RouterOutputs } from "~/server/api/root";
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
+import { api } from "~/trpc/react";
+
+export function OrganizationsList({
+  universityName,
+  promise,
+}: {
+  universityName: (typeof SCHOOLS)[number]["name"];
+  promise: Promise<RouterOutputs["organization"]["allByUniversityName"]>;
+}) {
+  const orgsFromServer = use(promise);
+  const allOrgs = api.organization.allByUniversityName.useQuery(
+    {
+      universityName,
+    },
+    {
+      initialData: orgsFromServer,
+    },
+  );
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const filteredEvents = allOrgs.data.filter((org) =>
+    org.name.toLowerCase().includes(searchTerm.toLowerCase()),
+  );
+
+  return (
+    <>
+      <input
+        type="text"
+        placeholder="Search events"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        className="flex h-9 w-1/2 rounded-md border border-input bg-card px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
+      />
+
+      {filteredEvents.length > 0 ? (
+        filteredEvents.map((event) => (
+          <Card
+            key={event.name}
+            className="w-[300px] transition-transform hover:scale-x-105 hover:cursor-pointer lg:w-[600px]"
+          >
+            <Link href={"/event/" + event.id}>
+              <CardHeader>
+                <CardTitle>{event.name}</CardTitle>
+                <CardDescription>{event.description}</CardDescription>
+              </CardHeader>
+            </Link>
+          </Card>
+        ))
+      ) : (
+        <div className="w-[300px] lg:w-[600px]">No events found.</div>
+      )}
+    </>
+  );
+}
--- src/app/(main)/components/organizations.tsx
+++ src/app/(main)/components/organizations.tsx
@@ -1,82 +1,25 @@
-"use client";
+import { redirect } from "next/navigation";
 
-import { useState } from "react";
-import Link from "next/link";
+import { SCHOOLS } from "~/consts";
+import { validateRequest } from "~/server/auth/validate-request";
+import { api } from "~/trpc/server";
+import { OrganizationsList } from "./organizations-list";
 
-import {
-  Card,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "~/components/ui/card";
+export default async function Events() {
+  const { user } = await validateRequest();
 
-export default function Organizations() {
-  const [searchTerm, setSearchTerm] = useState("");
+  if (!user) {
+    return redirect("/sign-in");
+  }
 
-  const filteredEvents = organizations.filter((org) =>
-    org.name.toLowerCase().includes(searchTerm.toLowerCase()),
-  );
+  const acronym = user.email
+    .split("@")[1]
+    ?.split(".")[0] as (typeof SCHOOLS)[number]["acronym"];
+  const school = SCHOOLS.find((school) => school.acronym === acronym)!;
 
-  return (
-    <>
-      <input
-        type="text"
-        placeholder="Search organizations"
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-        className="flex h-9 w-1/2 rounded-md border border-input bg-card px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
-      />
+  const promise = api.organization.allByUniversityName({
+    universityName: school.name,
+  });
 
-      {filteredEvents.length > 0 ? (
-        filteredEvents.map((org) => (
-          <Card
-            key={org.name}
-            className="w-[300px] transition-transform hover:scale-x-105 hover:cursor-pointer lg:w-[600px]"
-          >
-            <Link href={"/" + org.id}>
-              <CardHeader>
-                <CardTitle>{org.name}</CardTitle>
-                <CardDescription>{org.description}</CardDescription>
-              </CardHeader>
-            </Link>
-          </Card>
-        ))
-      ) : (
-        <div className="w-[300px] lg:w-[600px]">No events found.</div>
-      )}
-    </>
-  );
+  return <OrganizationsList universityName={school.name} promise={promise} />;
 }
-
-const organizations = [
-  {
-    name: "Meow Club",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    id: 1,
-  },
-  {
-    name: "Club Name 2",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    id: 2,
-  },
-  {
-    name: "Club Name 3",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    id: 3,
-  },
-  {
-    name: "Club Name 4",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    id: 4,
-  },
-  {
-    name: "Club Name 5",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    id: 5,
-  },
-];
